7a66d61c833657751dffa88366f790cb4e8ec8d4
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2023-12-21 12:47:24 +0100

    solved challenge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [holiday, setHoliday] = useState("");
+  const [date, setDate] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
+    const holidayValue = event.target.elements.holiday.value;
+    const dateValue = event.target.elements.date.value;
+
+    setHoliday(holidayValue);
+    setDate(dateValue);
+
+    event.target.reset();
   }
 
   return (
@@ -28,12 +38,12 @@ export default function App() {
           Submit
         </button>
       </form>
-      <h2>Output of Submitted Data</h2>
+      <h2>date</h2>
       <p>
-        Favourite Holiday: <span className="output">New Year</span>
+        Favourite Holiday: <span className="output">{holiday}</span>
       </p>
       <p>
-        Date: <span className="output">Well...</span>
+        Date: <span className="output">{date}</span>
       </p>
     </div>
   );
